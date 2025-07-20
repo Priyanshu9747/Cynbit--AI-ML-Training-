0aff5f44df3bb24be47a5223201a16cff3beb2fa
Priyanshu Sahu <sahupriyanshu.9747@gmail.com>
2025-07-20 17:03:59 +0530

    Update mini_student_report_card.py

--- mini_student_report_card.py
+++ mini_student_report_card.py
@@ -73,3 +73,41 @@ plt.ylabel('Number of Students')
 plt.grid(axis='y', linestyle='--', alpha=0.7)
 plt.tight_layout()
 plt.show()
+
+
+#output
+ How many students are in your class?  3
+
+ Entering details for Student 1:
+ Student Name: priyanshu
+   📘 Enter marks for Subject 1: 88
+   📘 Enter marks for Subject 2: 98
+   📘 Enter marks for Subject 3: 92
+
+ Entering details for Student 2:
+ Student Name: rahul
+   📘 Enter marks for Subject 1: 89
+   📘 Enter marks for Subject 2: 78
+   📘 Enter marks for Subject 3: 95
+
+ Entering details for Student 3:
+ Student Name: prince
+   📘 Enter marks for Subject 1: 90
+   📘 Enter marks for Subject 2: 67
+   📘 Enter marks for Subject 3: 78
+
+ Final Report Summary:
+ Total Students: {num_students}
+ Class Average Score: {df['Average'].mean():.2f}
+
+ Grade Distribution:
+Grade
+B    2
+A    1
+Name: count, dtype: int64
+
+ The report has been saved as 'student_report_card.csv'!
+
+ Generating Grade Distribution Chart...
+
+
